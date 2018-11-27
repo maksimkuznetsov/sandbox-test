2df8993dae8164ab4462b639f2d968d33c5cdfc7
Sandbox Sync <support@getsandbox.com>
2018-11-27 15:45:29 +0000

    Changes

--- sandbox/controllers/authController.js
+++ sandbox/controllers/authController.js
@@ -19,3 +19,22 @@ api.defineAction({
       res.json({ token: token })
     }
 })
+
+//create a user, body should be {email:"", password:""}
+api.defineAction({
+    path: '/api/0/auth/signup',
+    method: 'POST',
+    handler: function(req, res) {
+      var token = null;
+      
+      
+      if (state.activeUsers.length > 0) {
+          token = state.activeUsers[0].token;
+      } else {
+          token = 'a751a4a1-8710-439f-8165-f0f5d6d17e5d';
+          state.activeUsers.push({ token: token });
+      }
+      
+      res.json({ token: token })
+    }
+})
